728f6a323da696f464272a5157a10e9a60634aa2
WQM <244401893@qq.com>
2020-02-15 16:01:04 +0800

    重命名

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -25,8 +25,8 @@ public class OrderReceipt {
         output.append(orderInfo.getCustomerAddress());
 
         // prints lineItems
-        double totSalesTx = 0d;
-        double tot = 0d;
+        double totalSalesTax = 0d;
+        double totalAmount = 0d;
         for (Goods goods : orderInfo.getGoodsList()) {
             output.append(goods.getDescription());
             output.append('\t');
@@ -39,17 +39,17 @@ public class OrderReceipt {
 
             // calculate sales tax @ rate of 10%
             double salesTax = goods.totalAmount() * .10;
-            totSalesTx += salesTax;
+            totalSalesTax += salesTax;
 
             // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            tot += goods.totalAmount() + salesTax;
+            totalAmount += goods.totalAmount() + salesTax;
         }
 
         // prints the state tax
-        output.append("Sales Tax").append('\t').append(totSalesTx);
+        output.append("Sales Tax").append('\t').append(totalSalesTax);
 
         // print total amount
-        output.append("Total Amount").append('\t').append(tot);
+        output.append("Total Amount").append('\t').append(totalAmount);
         return output.toString();
     }
 }
